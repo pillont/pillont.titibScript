59e86016f299dddf5871680272964e9c642bc774
Thibaut PILLON <tpillon@hellowork.com>
2021-03-22 14:42:16 +0100

    WIP package.ts

---
+++ index.ts
@@ -0,0 +1,8 @@
+export * from './arrays.functions';
+export * from './async.functions';
+export * from './enum.functions';
+export * from './operators.functions';
+export * from './prompt.functions';
+export * from './string.functions';
+export * from './validation.functions';
+
